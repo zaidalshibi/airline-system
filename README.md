6dbda180e6c21b52c36c10dd53cb592ce1e44877
Zaid alshibi <zaidealshibi@gmail.com>
2022-10-31 03:53:07 +0300

    created the full airline system

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Airline-System
 
+## to start the system type node manager.js
+
 ![AirLine UML](./AirlineSystem.png)
--- events.js
+++ events.js
@@ -1,6 +1,6 @@
 'use strict';
 
 const Events = require('events');
-const events = new Events();
+const events = new Events.EventEmitter();
 
 module.exports = events;
